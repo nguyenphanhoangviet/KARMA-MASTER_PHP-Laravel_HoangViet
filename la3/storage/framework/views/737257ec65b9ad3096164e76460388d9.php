ccab60e3050e5fb24f2a49c1f523a420f3557101
nguyenphanhoangviet <135421436+nguyenphanhoangviet@users.noreply.github.com>
2024-07-08 04:34:08 +0700

    Delete la3 directory

--- la3/storage/framework/views/737257ec65b9ad3096164e76460388d9.php
+++
@@ -1,7 +0,0 @@
-
- <?php $__env->startSection('tieudetrang'); ?> Trang chủ tin tức
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('noidung'); ?>
-<h1>Đây là trang chủ</h1 <?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\lab1\la3\resources\views/home.blade.php ENDPATH**/ ?>
