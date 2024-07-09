599cec11d3a5578b9f3e8f87643776956736f799
nguyenphanhoangviet <viet.nguyenphanhoang@gmail.com>
2024-07-10 01:15:48 +0700

    Update 5 10/7/2024

--- lab5/resources/views/admin/product/show.blade.php
+++ lab5/resources/views/admin/product/show.blade.php
@@ -10,11 +10,11 @@
             <p><strong>Name:</strong> {{ $product->name }}</p>
             <p><strong>Description:</strong> {{ $product->description }}</p>
             <p><strong>Price:</strong> {{ $product->price }}</p>
-            <p><strong>Image:</strong><br><img src="{{ asset('imgs/products/' . $product->img) }}" alt="{{ $product->name }}" class="img-fluid"></p>
+            <p><strong>Image:</strong><br><img src="{{ asset('imgs/products/' . $product->img) }}" alt="{{ $product->name }}" class="img-fluid" height="300" width="300"></p>
             <p><strong>Category:</strong> {{ $product->category->name }}</p>
             <p><strong>Color:</strong> {{ $product->color->name ?? 'None' }}</p>
             <p><strong>Brand:</strong> {{ $product->brand->name }}</p>
         </div>
     </div>
 @endsection
-@endsection
+
