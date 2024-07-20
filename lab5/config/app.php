6933a97af79bf4207872e4c1a42ff17baab9c759
nguyenphanhoangviet <viet.nguyenphanhoang@gmail.com>
2024-07-21 01:24:40 +0700

    Update 17 21/7/2024

--- lab5/config/app.php
+++ lab5/config/app.php
@@ -153,14 +153,6 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        // App\Providers\EventServiceProvider::class,
-        // App\Providers\RouteServiceProvider::class,
-
-        // // Thêm dòng này
-        Rinvex\Categories\Providers\CategoriesServiceProvider::class,
     ],
 
     'aliases' => [
